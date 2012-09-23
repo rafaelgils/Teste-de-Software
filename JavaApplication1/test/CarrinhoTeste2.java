254e6ca25fec984c168b0c76b4a7d169a707645d
User <User@User-PC>
2012-09-22 22:31:49 -0300

    termino

--- JavaApplication1/test/CarrinhoTeste2.java
+++ JavaApplication1/test/CarrinhoTeste2.java
@@ -42,15 +42,23 @@ public class CarrinhoTeste2 {
 
 	@Test
 	public void colocandoMaisProdutos() throws CarrinhoVazioExpected {
+                Produto menor;
+                
 		Produto livro = new Produto("Java em 24 horas", 50.00);
 		carrinho.add(livro);
+                menor = carrinho.menorProduto();
+                assertArrayEquals(new Object[] { livro }, new Object[] { menor });
+                assertArrayEquals(new Object[] { livro }, new Object[] { carrinho.ultimoProduto() });
+                
 		Produto deitel = new Produto("Java: como programar", 150.00);
 		carrinho.add(deitel);
+                menor = carrinho.menorProduto();
+                assertArrayEquals(new Object[] { livro }, new Object[] { menor });
+                assertArrayEquals(new Object[] { deitel }, new Object[] { carrinho.ultimoProduto() });
+                
                 Produto caneta = new Produto("BIC",2.50);
                 carrinho.add(caneta);
-		Produto menor;
-		menor = carrinho.menorProduto();
-                assertArrayEquals(carrinho, actuals);
+                menor = carrinho.menorProduto();
 		assertArrayEquals(new Object[] { caneta }, new Object[] { menor });
                 assertArrayEquals(new Object[] { caneta }, new Object[] { carrinho.ultimoProduto() });
 	}
@@ -66,9 +74,48 @@ public class CarrinhoTeste2 {
                 carrinho.add(caneta);
 		Produto menor;
                 menor = carrinho.menorProduto();
-                assertArrayEquals(new Object[] { carrinho }, new Object[] {  });
+                assertArrayEquals(new Object[] { caneta }, new Object[] { menor });
+                assertArrayEquals(new Object[] { livro, deitel, caneta }, carrinho.getProdutos().toArray());
         }
         
+        @Test
+        public void removerProduto()
+        {
+            Produto livro = new Produto("Java em 24 horas", 50.00);
+            carrinho.removerProduto(livro);
+            assertArrayEquals( new Object[] {  } , carrinho.getProdutos().toArray());
+            
+            carrinho.add(livro);
+            
+            Produto deitel = new Produto("Javal: Como programar", 150.00);
+            carrinho.add(deitel);
+            
+            carrinho.removerProduto(livro);
+            
+            assertArrayEquals(new Object[] { deitel }, carrinho.getProdutos().toArray());            
+        }
+        
+        @Test
+        public void calcularTotal()
+        {   
+            assertEquals(0, carrinho.getTotal(), 0);
+            
+            Produto livro = new Produto("Java em 24 horas", 50.00);
+            carrinho.add(livro);
+            
+            assertEquals(50.00, carrinho.getTotal(), 0);
+            
+            Produto deitel = new Produto("Java: Como programar", 150.00);
+            carrinho.add(deitel);
+            
+            assertEquals(200.00, carrinho.getTotal(), 0);
+            
+            Produto BIC = new Produto("Caneta esferografica", 2.50);
+            carrinho.add(BIC);
+            
+            assertEquals(202.50, carrinho.getTotal(), 0);
+            
+        }
         
 	@Test
 	public void identidadeDeProdutos() throws CarrinhoVazioExpected {
